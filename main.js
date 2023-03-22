2cf89b0c695e9decaac4d560a7f55e25a0601b2e
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-03-22 14:27:30 +0100

    Finish adding button and description for each image

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ burgerIcon.addEventListener('click', () => {
 
 
 
-function toggleVisibility() {
+function toggleVisibilityLaGourmandise() {
   const paragraphe = document.getElementById("descriptionLaGourmandise");
   if (paragraphe.style.display === "none") {
     paragraphe.style.display = "block";
@@ -18,11 +18,75 @@ function toggleVisibility() {
   }
 }
 
-// function toggleVisibility() {
-//   const paragraphe = document.getElementById("descriptionLaLoupiote");
-//   if (paragraphe.style.display === "none") {
-//     paragraphe.style.display = "block";
-//   } else {
-//     paragraphe.style.display = "none";
-//   }
-// }
+function toggleVisibilityLaLoupiote() {
+  const paragraphe = document.getElementById("descriptionLaLoupiote");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilityRitalCafé() {
+  const paragraphe = document.getElementById("descriptionRitalCafé");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilityMcdo() {
+  const paragraphe = document.getElementById("descriptionMcdo");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilityrestoRapide() {
+  const paragraphe = document.getElementById("descriptionrestoRapide");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilitypizzaDiNapoli() {
+  const paragraphe = document.getElementById("descriptionpizzaDiNapoli");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilitysaveursEtTraditions() {
+  const paragraphe = document.getElementById("descriptionsaveursEtTraditions");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilityaurelienEtSolene() {
+  const paragraphe = document.getElementById("descriptionaurelienEtSolene");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
+function toggleVisibilitygrosChene() {
+  const paragraphe = document.getElementById("descriptiongrosChene");
+  if (paragraphe.style.display === "none") {
+    paragraphe.style.display = "block";
+  } else {
+    paragraphe.style.display = "none";
+  }
+}
+
